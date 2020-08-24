0588dbaaf17d61933b3317cdc4e17e5b44e7993b
Abhishek <34632758+Abhi03021990@users.noreply.github.com>
2020-08-24 20:48:24 +0530

    new code
    24th aug logical operator eg

---
+++ LogicalOperatorsEg.java
@@ -0,0 +1,16 @@
+class LogicalOperatorsEg{
+	public static void main(String args[]){
+		//relational operators eg > <
+		int a = 10;
+		int b = 20;
+		if(a>b){
+			System.out.print("out of a and b , a is greatest");
+		}else{
+			System.out.print("out of a and b , b is greatest");
+		}
+			
+			
+		
+	}
+}
+		
